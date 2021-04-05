24465e769227d1d7c7d4039a4d0fefe4852f5222
Olga Kokkareva <olgakokkareva08055@gmail.com>
2021-04-05 20:15:41 +0300

    change dictionary

---
+++ downloadDictionary.js
@@ -0,0 +1,43 @@
+let dictionary = null;
+const downloadDictionary = (url, file, callback) => {
+    let stream = fs.createWriteStream(file);
+    let req = https.get(url, function (res) {
+        res.pipe(stream);
+        stream.on('finish', function () {
+            stream.close(callback);
+            console.log('dictionary downloaded');
+        });
+    }).on('error', function (err) {
+        fs.unlink(file);
+        if (callback) cb(err.message);
+    });
+};
+
+let loadDictionary = (file, callback) => {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+            return;
+        }
+        dictionary = JSON.parse(data);
+        console.log('dictionary loaded.');
+        callback();
+    })
+};
+
+downloadDictionary('https://raw.githubusercontent.com/kkkrv/dictionary/main/dictionary.json', 'dictionary.json', (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    loadDictionary('dictionary.json', (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('ready to serve');
+    });
+});
+
+module.exports = dictionary;
