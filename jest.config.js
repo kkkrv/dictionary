68a2ba2fcd1dbbf8c1c034aa94ecf54cc9e153ee
Olga Kokkareva <olgakokkareva08055@gmail.com>
2021-04-06 11:14:37 +0300

    change style according to eslint rules

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    setupFilesAfterEnv: ['./jest.setup.js']
-}
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
 
 process.env = Object.assign(process.env, {
-    NODE_ENV: 'test'
+  NODE_ENV: 'test',
 });
